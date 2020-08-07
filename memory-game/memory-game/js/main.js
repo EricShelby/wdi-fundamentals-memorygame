094513094b61d47534e9e15fe456970f933bd8a9
Eric Shelby <minischloob@gmail.com>
2020-08-07 00:56:00 -0600

    created game logic

--- memory-game/memory-game/js/main.js
+++ memory-game/memory-game/js/main.js
@@ -1,7 +1,27 @@
 console.log("Up and running!");
-let cardOne = "Queen";
-let cardTwo = "Queen";
-let cardThree = "King";
-let cardFour = "King";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+const cards= ["queen","queen","king","king"];
+
+const cardsInPlay= [];
+
+var cardOne;
+
+cards[0] = 'cardOne';
+
+cardsInPlay.push('cardOne');
+console.log("User flipped queen")
+
+cards[1] = 'cardTwo';
+
+cardsInPlay.push('cardTwo');
+console.log("User flipped queen")
+
+cards[2] = 'cardThree';
+
+cardsInPlay.push('cardThree');
+console.log("User flipped king")
+
+cards[3] = 'cardFour';
+
+cardsInPlay.push('cardFour');
+console.log("User flipped king")
